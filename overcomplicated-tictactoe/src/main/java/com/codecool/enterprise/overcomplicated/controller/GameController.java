234470464f1e22a932246c58d8321cebe13b2c35
kelliartamas <kelliar.tamas@gmail.com>
2017-12-31 10:13:16 +0100

    Adding the main app-controlling methods

--- overcomplicated-tictactoe/src/main/java/com/codecool/enterprise/overcomplicated/controller/GameController.java
+++ overcomplicated-tictactoe/src/main/java/com/codecool/enterprise/overcomplicated/controller/GameController.java
@@ -1,50 +1,114 @@
 package com.codecool.enterprise.overcomplicated.controller;
 
 import com.codecool.enterprise.overcomplicated.model.Player;
-import com.codecool.enterprise.overcomplicated.model.TictactoeGame;
+import com.codecool.enterprise.overcomplicated.service.TictactoeGame;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JacksonJsonParser;
+import org.springframework.boot.json.JsonParser;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Controller
 @SessionAttributes({"player", "game"})
 public class GameController {
 
-    @ModelAttribute("player")
-    public Player getPlayer() {
-        return new Player();
-    }
+	@Autowired
+	private TictactoeGame tictactoeGame;
 
-    @ModelAttribute("game")
-    public TictactoeGame getGame() {
-        return new TictactoeGame();
-    }
 
-    @ModelAttribute("avatar_uri")
-    public String getAvatarUri() {
-        return "https://robohash.org/codecool";
-    }
+	@ModelAttribute("player")
+	public Player getPlayer() {
+		return new Player();
+	}
 
-    @GetMapping(value = "/")
-    public String welcomeView(@ModelAttribute Player player) {
-        return "welcome";
-    }
+	@ModelAttribute("game")
+	public TictactoeGame getGame() {
+		return new TictactoeGame();
+	}
 
-    @PostMapping(value="/changeplayerusername")
-    public String changPlayerUserName(@ModelAttribute Player player) {
-        return "redirect:/game";
-    }
+	@GetMapping(value = "/")
+	public String welcomeView(@ModelAttribute Player player) {
+		return "welcome";
+	}
 
-    @GetMapping(value = "/game")
-    public String gameView(@ModelAttribute("player") Player player, Model model) {
-        model.addAttribute("funfact", "&quot;Chuck Norris knows the last digit of pi.&quot;");
-        model.addAttribute("comic_uri", "https://imgs.xkcd.com/comics/bad_code.png");
-        return "game";
-    }
+	@PostMapping(value = "/changeplayerusername")
+	public String changPlayerUserName(@ModelAttribute Player player) {
+		return "redirect:/game";
+	}
 
-    @GetMapping(value = "/game-move")
-    public String gameMove(@ModelAttribute("player") Player player, @ModelAttribute("move") int move) {
-        System.out.println("Player moved " + move);
-        return "redirect:/game";
-    }
+	@GetMapping(value = "/game")
+	public String gameView(@ModelAttribute("player") Player player, Model model) {
+		List<Integer> moveList = tictactoeGame.getPlayerMoveList();
+		List<Integer> computerMoves = tictactoeGame.getComputerMoveList();
+		String winner = tictactoeGame.getWinner();
+		model.addAttribute("winner", winner);
+		model.addAttribute("moveList", moveList);
+		model.addAttribute("computerMoves", computerMoves);
+		return "game";
+	}
+
+	@GetMapping(value = "/game-move")
+	public String gameMove(@ModelAttribute("player") Player player, @ModelAttribute("move") int move) {
+		tictactoeGame.playerMove(move);
+		tictactoeGame.gameState();
+		try {
+			RestTemplate restTemplate = new RestTemplate();
+			ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:60003/ai?gameState=" + tictactoeGame.getGameState(), String.class);
+			JsonParser parser = new JacksonJsonParser();
+			int aiMove = (Integer) parser.parseMap(response.getBody()).get("move");
+			tictactoeGame.computerMove(aiMove);
+			return "redirect:/game";
+		} catch (Exception e) {
+			e.getMessage();
+			return "redirect:/game";
+		}
+	}
+
+	@ModelAttribute("funfact")
+	public String funFact() {
+		try {
+			RestTemplate restTemplate = new RestTemplate();
+			ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:60000/funfact", String.class);
+			JsonParser jsonParser = new JacksonJsonParser();
+			return (String) jsonParser.parseMap(response.getBody()).get("funfact");
+		} catch (ResourceAccessException e) {
+			e.getMessage();
+			return "Error";
+		}
+	}
+
+	@ModelAttribute("avatar_uri")
+	public String getAvatarUri(@ModelAttribute Player player) {
+		try {
+			RestTemplate restTemplate = new RestTemplate();
+			ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:60001/avatar?avatarString=" + player.getUserName(), String.class);
+			JsonParser jsonParser = new JacksonJsonParser();
+			return (String) jsonParser.parseMap(response.getBody()).get("uri");
+		} catch (ResourceAccessException e) {
+			e.getMessage();
+			return "Error";
+		}
+	}
+
+	@ModelAttribute("comic_uri")
+	public String getComic() {
+		try {
+			RestTemplate restTemplate = new RestTemplate();
+			ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:60002/comics", String.class);
+			JsonParser jsonParser = new JacksonJsonParser();
+			return (String) jsonParser.parseMap(response.getBody()).get("uri");
+		} catch (Exception e) {
+			e.getMessage();
+			return "Error";
+		}
+	}
 }
